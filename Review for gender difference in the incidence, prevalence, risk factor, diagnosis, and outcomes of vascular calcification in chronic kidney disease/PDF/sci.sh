9d150b98e530bd8bb2643b428c9432a1db95d822
patrickwoolf <h211133@nehs.hc.edu.tw>
2021-02-26 16:20:39 +0800

    20210226-1620

--- Review for gender difference in the incidence, prevalence, risk factor, diagnosis, and outcomes of vascular calcification in chronic kidney disease/PDF/sci.sh
+++ Review for gender difference in the incidence, prevalence, risk factor, diagnosis, and outcomes of vascular calcification in chronic kidney disease/PDF/sci.sh
@@ -3,18 +3,19 @@
 # echo $(curl https://sci-hub.se/"$@" | grep -ohP 'https\:\/.*?\.pdf')
 sci="https://sci-hub.se/"
 doi=(
-10.1111/j.1525-1594.2009.00814.x
-10.1111/j.1540-8191.2007.00431.x
+10.2174/157016108783955374
 )
 
+rm scipdf.txt
 for f in ${doi[@]};
 do
   DOI=$(curl ${sci}${f} | grep -ohP 'https\:\/.*?\.pdf' | head -1)
-  echo $DOI
-    if [ "$DOI" ]; then
-      wget  --user-agent="Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1"  -l1 --no-parent $DOI 2>/dev/null
-    else
-      echo "No pdf for $f"
-    fi
-
+  if [ ! "$(curl ${sci}${f} | grep -hP 'http.*?pdf')" ]; then echo 'https://'$(curl ${sci}${f} | grep -ohP 'sci.*?pdf' | head -1) >> scipdf.txt; fi
+  echo $DOI >> scipdf.txt
+#    if [ "$DOI" ]; then
+#      wget  --user-agent="Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1"  -l1 --no-parent $DOI 2>/dev/null
+#    else
+#      echo "No pdf for $f"
+#    fi
 done
+wget -i scipdf.txt
